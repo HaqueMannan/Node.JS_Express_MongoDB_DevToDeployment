3bf45168b8a545bfa76b5cb9633ccc0168d8949a
HaqueMannan <34745319+HaqueMannan@users.noreply.github.com>
2021-04-17 16:16:49 +0100

    NodeJS, Express & MongoDB Dev to Deployment (Project Based Learning): StoryBooks App: Initial Setup and Google OAuth - Creating the User Model.

--- Projects/StoryBooks/app.js
+++ Projects/StoryBooks/app.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const passport = require('passport');
+const keys = require('./config/keys');
 
 // Setup Express Server:
 const app = express();
 const port = process.env.PORT || 5000;
+
+// Map Global Promises (remove any weird promise warnings):
+mongoose.Promise = global.Promise;
+// Mongoose Connection:
+mongoose.connect(keys.mongoURI, {
+   useNewUrlParser: true,
+   useUnifiedTopology: true
+}).then(() => {
+   console.log('MongoDB Connect')
+}).catch(err => {
+   console.log(err);
+});
 
 // Load Passport Config:
 require('./config/passport')(passport);
