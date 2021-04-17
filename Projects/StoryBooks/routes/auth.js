d5299ef4dd06102693854bd4441d4866a0eb79a4
HaqueMannan <34745319+HaqueMannan@users.noreply.github.com>
2021-04-17 16:02:01 +0100

    NodeJS, Express & MongoDB Dev to Deployment (Project Based Learning): StoryBooks App: Initial Setup and Google OAuth - Auth Callback.

--- Projects/StoryBooks/routes/auth.js
+++ Projects/StoryBooks/routes/auth.js
@@ -4,4 +4,10 @@ const passport = require('passport');
 
 router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
 
+router.get('/google/callback',
+   passport.authenticate('google', { failureRedirect: '/' }), (req, res) => {
+      res.redirect('/dashboard');
+   }
+);
+
 module.exports = router;
